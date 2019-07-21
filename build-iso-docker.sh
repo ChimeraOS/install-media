d31e3b1fde4977c4b60789c177f93b4b330d12bb
Wouter Wijsman <wwijsman@live.nl>
2019-07-22 01:39:08 +0200

    Moved installation media creation to new repo

---
+++ build-iso-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# configuration variables for the iso
+dockerfile="docker/Dockerfile"
+output_dir="output"
+
+# get the directory of this script
+work_dir="$(realpath $0|rev|cut -d '/' -f2-|rev)"
+
+# create output directory if it doesn't exist yet
+mkdir -p ${work_dir}/${output_dir}
+
+# build the docker container
+docker build -f ${work_dir}/${dockerfile} -t gameros-builder ${work_dir}
+
+# make the container build the iso
+exec docker run --privileged --rm -ti -v ${work_dir}/${output_dir}:/root/gameros/out -h gameros-builder gameros-builder ./build.sh -v
---
+++ build-iso.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# configuration variables for the iso
+output_dir="output"
+script_dir="gameros"
+
+# get the directory of this script
+work_dir="$(realpath $0|rev|cut -d '/' -f2-|rev)"
+
+# create output directory if it doesn't exist yet
+mkdir -p ${work_dir}/${output_dir}
+
+# change working directory
+cd ${work_dir}/${script_dir}
+
+# make the container build the iso
+exec ./build.sh -v -o ${work_dir}/${output_dir}
