9e7518c75a6c0126e87f1dd73d8ce1e6b7a742e8
Wouter Wijsman <wwijsman@live.nl>
2019-08-24 13:44:58 +0200

    Updated build scripts They now remove the old work directory, make sure the root user is used and try to change the pacman.conf back when done

--- build-iso-docker.sh
+++ build-iso-docker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $EUID -ne 0 ]; then
+	echo "$(basename $0) must be run as root"
+	exit 1
+fi
+
 # configuration variables for the iso
 dockerfile="docker/Dockerfile"
 output_dir="output"
@@ -11,6 +16,10 @@ work_dir="$(realpath $0|rev|cut -d '/' -f2-|rev)"
 # create output directory if it doesn't exist yet
 mkdir -p ${work_dir}/${output_dir}
 
+# remove the work directory used for the previous build
+rm -rf work
+
+# add the local repo
 sed -i "s?Server = localrepo?Server = file://${work_dir}/${repo_dir}?" ${work_dir}/${script_dir}/pacman.conf
 
 # build the docker container
@@ -18,3 +27,6 @@ docker build -f ${work_dir}/${dockerfile} -t gameros-builder ${work_dir}
 
 # make the container build the iso
 exec docker run --privileged --rm -ti -v ${work_dir}/${output_dir}:/root/gameros/out -h gameros-builder gameros-builder ./build.sh -v
+
+# bring pacman.conf back in its original state
+sed -i "s?Server = file://${work_dir}/${repo_dir}?Server = localrepo?" ${work_dir}/${script_dir}/pacman.conf
--- build-iso.sh
+++ build-iso.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $EUID -ne 0 ]; then
+	echo "$(basename $0) must be run as root"
+	exit 1
+fi
+
 # configuration variables for the iso
 output_dir="output"
 script_dir="gameros"
@@ -11,10 +16,17 @@ work_dir="$(realpath $0|rev|cut -d '/' -f2-|rev)"
 # create output directory if it doesn't exist yet
 mkdir -p ${work_dir}/${output_dir}
 
+# add the local repo
 sed -i "s?Server = localrepo?Server = file://${work_dir}/${repo_dir}?" ${work_dir}/${script_dir}/pacman.conf
 
 # change working directory
 cd ${work_dir}/${script_dir}
 
+# remove the work directory used for the previous build
+rm -rf work
+
 # make the container build the iso
 exec ./build.sh -v -o ${work_dir}/${output_dir}
+
+# bring pacman.conf back in its original state
+sed -i "s?Server = file://${work_dir}/${repo_dir}?Server = localrepo?" ${work_dir}/${script_dir}/pacman.conf
