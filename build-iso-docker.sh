f05d623ce90ac7b15e35f363514fa84791061131
Wouter Wijsman <wwijsman@live.nl>
2019-08-24 12:44:09 +0200

    Added local repo to pacman config

--- build-iso-docker.sh
+++ build-iso-docker.sh
@@ -3,12 +3,15 @@
 # configuration variables for the iso
 dockerfile="docker/Dockerfile"
 output_dir="output"
+repo_dir="packages"
 
 # get the directory of this script
 work_dir="$(realpath $0|rev|cut -d '/' -f2-|rev)"
 
 # create output directory if it doesn't exist yet
 mkdir -p ${work_dir}/${output_dir}
+
+sed -i "s?Server = localrepo?Server = file://${work_dir}/${repo_dir}?" ${work_dir}/${script_dir}/pacman.conf
 
 # build the docker container
 docker build -f ${work_dir}/${dockerfile} -t gameros-builder ${work_dir}
--- build-iso.sh
+++ build-iso.sh
@@ -3,12 +3,15 @@
 # configuration variables for the iso
 output_dir="output"
 script_dir="gameros"
+repo_dir="packages"
 
 # get the directory of this script
 work_dir="$(realpath $0|rev|cut -d '/' -f2-|rev)"
 
 # create output directory if it doesn't exist yet
 mkdir -p ${work_dir}/${output_dir}
+
+sed -i "s?Server = localrepo?Server = file://${work_dir}/${repo_dir}?" ${work_dir}/${script_dir}/pacman.conf
 
 # change working directory
 cd ${work_dir}/${script_dir}
