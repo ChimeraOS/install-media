0d7eded3b6846e41e936a4a96b062f402c9c98a9
liberodark <liberodark@gmail.com>
2020-04-15 13:50:21 +0200

    Update build-iso.sh
    Other fix too.

--- build-iso.sh
+++ build-iso.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $EUID -ne 0 ]; then
-	echo "$(basename $0) must be run as root"
+	echo "$(basename "$0") must be run as root"
 	exit 1
 fi
 
@@ -10,13 +10,13 @@ output_dir="output"
 script_dir="gameros"
 
 # get the directory of this script
-work_dir="$(realpath $0|rev|cut -d '/' -f2-|rev)"
+work_dir="$(realpath "$0"|rev|cut -d '/' -f2-|rev)"
 
 # create output directory if it doesn't exist yet
-mkdir -p ${work_dir}/${output_dir}
+mkdir -p "${work_dir}"/${output_dir}
 
 # change working directory
-cd ${work_dir}/${script_dir}
+cd "${work_dir}"/${script_dir} || exit
 
 # make the container build the iso
-exec ./build.sh -v -o ${work_dir}/${output_dir}
+exec ./build.sh -v -o "${work_dir}"/${output_dir}
