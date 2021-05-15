3698c8ccc713c019346c9fdbdc4d71e5003815bb
Wouter Wijsman <wwijsman@live.nl>
2021-05-15 15:39:34 +0200

    Update build-iso.sh for new archiso version

--- build-iso.sh
+++ build-iso.sh
@@ -5,18 +5,15 @@ if [ $EUID -ne 0 ]; then
 	exit 1
 fi
 
-# configuration variables for the iso
-output_dir="output"
-script_dir="gameros"
-
 # get the directory of this script
 work_dir="$(realpath $0|rev|cut -d '/' -f2-|rev)"
 
-# create output directory if it doesn't exist yet
-mkdir -p ${work_dir}/${output_dir}
+# configuration variables for the iso
+output_dir="${work_dir}/output"
+script_dir="${work_dir}/gameros"
 
-# change working directory
-cd ${work_dir}/${script_dir}
+# create output directory if it doesn't exist yet
+mkdir -p "${output_dir}"
 
 # make the container build the iso
-exec ./build.sh -v -o ${work_dir}/${output_dir}
+exec mkarchiso -v -w "${work_dir}" -o "${output_dir}" "${script_dir}"
