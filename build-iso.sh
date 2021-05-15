2288fa88e4107d805bbc4c38fa615662d5e0dd44
Wouter Wijsman <wwijsman@live.nl>
2021-05-15 16:22:09 +0200

    Add temp dir to work in

--- build-iso.sh
+++ build-iso.sh
@@ -11,9 +11,13 @@ work_dir="$(realpath $0|rev|cut -d '/' -f2-|rev)"
 # configuration variables for the iso
 output_dir="${work_dir}/output"
 script_dir="${work_dir}/gameros"
+temp_dir="${work_dir}/temp"
 
 # create output directory if it doesn't exist yet
 mkdir -p "${output_dir}"
 
+rm -rf "${temp_dir}"
+mkdir -p "${temp_dir}"
+
 # make the container build the iso
-exec mkarchiso -v -w "${work_dir}" -o "${output_dir}" "${script_dir}"
+exec mkarchiso -v -w "${temp_dir}" -o "${output_dir}" "${script_dir}"
