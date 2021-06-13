104e47c24bf3c1eac15660675e632037801b852c
Joaquín Ignacio Aramendía <samsagax@gmail.com>
2021-06-13 19:19:15 -0300

    Add NetworkManager support during installation
    - Add `networkmanager` package to the iso
    - Remove `iwd` service that could interfere with NM
    - Add `NetworkManager` services to start at boot
    - Ask the user for configuration of the network if it doesn't detect
      one during install.
    - Minor fix: retab from tabs to spaces.
    
    Note: changes to frzr post-install are needed for this to fully work in
    the installed system (i.e. nm system connections need to be copied over)

--- gameros/airootfs/root/install.sh
+++ gameros/airootfs/root/install.sh
@@ -2,38 +2,38 @@
 
 
 if [ $EUID -ne 0 ]; then
-	echo "$(basename $0) must be run as root"
-	exit 1
+    echo "$(basename $0) must be run as root"
+    exit 1
 fi
 
-#### Internet connection detection ####
-dhcpcd --background
-
+#### Wait for conenction or ask the user for configuration ####
 whiptail --infobox "Checking connection..." 10 50
 sleep 5
 
 while ! ( curl -Is https://gamer-os.github.io/ | head -1 | grep 200 > /dev/null ); do
-	whiptail --yesno "No internet connection detected. Please connect this computer\
-	 to the internet with a wired connection, wait a few seconds, then retry." 10 50\
-	 --yes-button "Retry"\
-	 --no-button "Exit"
+    whiptail --yesno "No wired connection detected. Please connect this computer \
+     to the internet by configuring a new network." 10 50 \
+     --yes-button "Configure" \
+     --no-button "Exit"
 
-	if [ $? -ne 0 ]; then
-		 exit 1
-	fi
+    if [ $? -ne 0 ]; then
+         exit 1
+    fi
+
+    nmtui
 done
 #######################################
 
 if ! frzr-bootstrap gamer; then
-	exit
+    exit 1
 fi
 
 export SHOW_UI=1
 if ! frzr-deploy gamer-os/gamer-os:stable; then
-	echo "Installation failed."
-	exit
+    echo "Installation failed."
+    exit 1
 fi
 
 if (whiptail --yesno "Installation complete. Would you like to restart now?" 10 50); then
-	reboot
+    reboot
 fi
