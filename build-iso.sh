19477852219508ba51104200bfbb5d3eedbb60f6
Joaquín Ignacio Aramendía <samsagax@gmail.com>
2021-06-16 14:20:27 -0300

    Add package `frzr` to the image
    - Add `pikaur` to the packages needed to build the image.
    - Add a generic way to add packages built from the AUR. More packages
      can be added by changing the `AUR_PACKAGES` variable in
      `build-iso.sh`.
    - Bugfix: Make docker container able to use pikaur.
    - Add a custom repo to the build made from AUR packages. `pacman.conf`
      now is a template that the script needs to recreate.
    - Add `frzr` to the list of packages

--- build-iso.sh
+++ build-iso.sh
@@ -19,5 +19,28 @@ mkdir -p "${output_dir}"
 rm -rf "${temp_dir}"
 mkdir -p "${temp_dir}"
 
+# add AUR packages to the build
+AUR_PACKAGES=frzr
+
+# create repo directory if it doesn't exist yet
+LOCAL_REPO="${script_dir}/extra_pkg"
+mkdir -p ${LOCAL_REPO}
+
+PIKAUR_CMD="PKGDEST=/tmp/temp_repo pikaur --noconfirm -Sw ${AUR_PACKAGES}"
+PIKAUR_RUN=(bash -c "${PIKAUR_CMD}")
+if [ -n "${BUILD_USER}" ]; then
+	PIKAUR_RUN=(su "${BUILD_USER}" -c "${PIKAUR_CMD}")
+fi
+
+# build packages to the repo
+"${PIKAUR_RUN[@]}"
+
+# copy all built packages to the repo
+cp /tmp/temp_repo/* ${LOCAL_REPO}
+
+# Add the repo to the build
+repo-add ${LOCAL_REPO}/gameros.db.tar.gz ${LOCAL_REPO}/*.pkg.*
+sed "s|LOCAL_REPO|$LOCAL_REPO|g" $script_dir/pacman.conf.template > $script_dir/pacman.conf
+
 # make the container build the iso
 exec mkarchiso -v -w "${temp_dir}" -o "${output_dir}" "${script_dir}"
--- gameros/airootfs/root/install.sh
+++ gameros/airootfs/root/install.sh
@@ -24,14 +24,6 @@ while ! ( curl -Is https://gamer-os.github.io/ | head -1 | grep 200 > /dev/null 
 done
 #######################################
 
-#### Install frzr
-curl -L https://github.com/gamer-os/frzr/releases/download/0.7.0/frzr-0.7.0-1-any.pkg.tar.zst > frzr.pkg.tar.zst
-pacman --noconfirm -U frzr.pkg.tar.zst
-if [ $? -ne 0 ]; then
-	exit 1
-fi
-rm frzr.pkg.tar.zst
-
 if ! frzr-bootstrap gamer; then
 	exit
 fi
