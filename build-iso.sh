74321708211f6fd2136dcd56d3ededf82bce9cdb
Alesh Slovak <aleshslovak@gmail.com>
2021-08-21 17:22:42 -0400

    fix failure to get iso file name

--- build-iso.sh
+++ build-iso.sh
@@ -50,7 +50,7 @@ sed "s|LOCAL_REPO|$LOCAL_REPO|g" $script_dir/pacman.conf.template > $script_dir/
 # make the container build the iso
 mkarchiso -v -w "${temp_dir}" -o "${output_dir}" "${script_dir}"
 
-ISO_FILE_PATH=`ls "${output_dir}/*.iso"`
+ISO_FILE_PATH=`ls ${output_dir}/*.iso`
 ISO_FILE_NAME=`basename "${ISO_FILE_PATH}"`
 VERSION=`echo "${ISO_FILE_NAME}" | cut -c11-20 | sed 's/\./-/g'`
 ID=`git rev-parse --short HEAD`
