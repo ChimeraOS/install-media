e6e1d6f203d84f03754316c1192085c6d99975a2
Alesh Slovak <aleshslovak@gmail.com>
2022-05-27 08:46:03 -0400

    force fresh build every time

--- build-iso-docker.sh
+++ build-iso-docker.sh
@@ -6,8 +6,11 @@ work_dir="$(realpath $0|rev|cut -d '/' -f2-|rev)"
 # configuration variables for the iso
 dockerfile="${work_dir}/docker/Dockerfile"
 
+# fetch latest base docker image
+docker pull archlinux:base-devel
+
 # build the docker container
-docker build -f "${dockerfile}" -t chimera-install-builder ${work_dir}
+docker build --no-cache -f "${dockerfile}" -t chimera-install-builder ${work_dir}
 
 # make the container build the iso
 exec docker run --privileged --rm -v ${work_dir}:/root/chimeraos -h chimera-install-builder chimera-install-builder ./build-iso.sh
